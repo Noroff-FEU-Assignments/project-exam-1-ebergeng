516f4d995a5b95f74855670dcb2f97f609185caf
ebergeng <59417456+ebergeng@users.noreply.github.com>
2023-02-06 16:37:07 +0100

    ds
    ds

--- scripts/api_call.js
+++ scripts/api_call.js
@@ -1,10 +1,12 @@
 const postWrapper = document.querySelector(".wrapper");
 var mobileSize = window.matchMedia("(max-width: 1000px)");
+const morePosts = document.querySelector(".more_posts");
+const filter = document.querySelector(".filter");
 
 
 let posts = []
-let pos = 0
-
+let pos = 1
+let mobile = true
 
 const baseUrl = "https://myflashcard.org/wp-json/";
 const postUrl = "wp/v2/posts"
@@ -26,121 +28,93 @@ async function apiCall() {
     }
     
     if(mobileSize.matches) {
-        updatePage_mobile()
+        mobile = true
+        pos = 1
+        filter.style.display = "none"
     }
 
     else {
-        updatePage_desktop()
+        mobile = false
+        morePosts.style.display = "none"
+        pos = 0
     }
-
-
     mobileSize.addListener(check_screen_size)
-
-
-    
+    add_post_to_page()
 }
-
 
 function generate_posts(data) {
     let post = {}
-    let id = data.id
-    let img = data.yoast_head_json.og_image[0].url;
-    let title = data.title.rendered;
+    post.id = data.id
+    post.img = data.yoast_head_json.og_image[0].url;
+    post.title = data.title.rendered;
     let sum = ""
     for(let i = 0; i < 150; i++) {
         sum += data.excerpt.rendered[i]
     }
-    sum += "</p>"
-
+    sum += "..</p>"
+    post.sum = sum
     let date = ""
     for(let i = 0; i < 10; i++) {
         date += data.date[i]
     }
-
-    post.id = id
-    post.img = img
-    post.title =  title
-    post.sum = sum
     post.date = date
     posts.push(post)
-
 }
 
 
-function createHTML(img, title, sum, date) {
-    let HTML = `<a href="index.html" class="post-wrapp">
-                    <div class="img-holder">
-                        <img src="${img}" alt="">
-                    </div>
-                    <div class="text-holder">
-                        <h1>${title}</h1>
-                        ${sum}
-                        <p class="date">${date}</p>
-                    </div>
-                </a>`
-                
-    return HTML
-}
-
-
-
-
-function updatePage_mobile() {
-
-    try {
-        for(let i = pos; i < pos + 3; i++) {
-            postWrapper.innerHTML += createHTML(posts[i].img, posts[i].title, posts[i].sum, posts[i].date)
-        }
-    }
-    catch(err) {
-        console.log("no more posts")
-    } 
-}
-
-
-
-function updatePage_desktop() {
-    postWrapper.innerHTML = `<div class="carusel">
-                                <div class="filter"></div>
-                                <div class="carusel-content"></div>
-                            </div>`
+function add_post_to_page() {
+    const loader = document.querySelector(".big_load");
     const carusel = document.querySelector(".carusel")
-    carusel.style.backgroundImage = `url(${posts[pos].img})`
+    if(!mobile){
+        carusel.style.backgroundImage = `url(${posts[pos].img})`
+    }
+    
 
     const caruselContent = document.querySelector(".carusel-content")
     let className;
     let headline;
-    for(let i = 0; i < 3; i++){ 
-        if(i===0) {
-            className = "old"
-            try {
-                headline = `<h3>${posts[pos + i - 1].title}</h3>`
+    for(let i = 0; i < 3; i++){
+        try {
+            if(!mobile) {
+                if(i===0) {
+                    className = "old"
+                    try {
+                        headline = `<h3>${posts[pos + i - 1].title}</h3>`
+                    }
+                    catch(err) {
+                        console.log(err)
+                    }        
+                }
+                else if(i===1){
+                    className = "current"
+                    headline = `<h2>${posts[pos + i - 1].title}</h2>`
+                }
+                else{
+                    className = "next"
+                    try {
+                        headline = `<h3>${posts[pos + i - 1].title}</h3>`
+                    }
+                    catch(err) {
+                        console.log(err)
+                    }
+                }
             }
-            catch(err) {
-                console.log(err)
-            }
-            
-            
+            else {
+                headline = `<h2>${posts[pos + i - 1].title}</h2>`
+                className = "current"
+            }  
         }
-        else if(i===1){
-            className = "current"
-            headline = `<h2>${posts[pos + i - 1].title}</h2>`
-        }
-        else{
-            className = "next"
-            try {
-                headline = `<h3>${posts[pos + i - 1].title}</h3>`
-            }
-            catch(err) {
-                console.log(err)
-            }
+        catch(err) {
+            console.log("no more posts")
+            morePosts.style.display = "none"
         }
 
+
         try{
-            caruselContent.innerHTML += `<div class="text-holder ${className}">
+            caruselContent.innerHTML += `<div class="post-wrapper ${className}" id="${posts[pos + i - 1].id}">
                                     ${headline}
                                     <div class="img-holder"><img src="${posts[pos + i - 1].img}" alt=""></div>
-                                    ${posts[pos + i - 1].sum + `<p class="read-more">Read more..</p>`}
+                                    ${posts[pos + i - 1].sum +`<p class="read-more">Read more..</p>`}
                                     
                                     <div class="post-info">
                                         <p class="date">Posted: ${posts[pos + i - 1].date}</p>
@@ -152,6 +126,7 @@ function updatePage_desktop() {
                                         </div>
                                     </div>
                                 </div>`
+            loader.style.display = "none";
                                 
         }
         catch(err) {
@@ -163,41 +138,46 @@ function updatePage_desktop() {
         })
     }
 
-    let old = document.querySelector(".old");
-    old.addEventListener("click", function() {
-        pos -= 1
-        updatePage_desktop()
-    })
+    if(!mobile) {
+        let old = document.querySelector(".old");
+        old.addEventListener("click", function() {
+            pos -= 1
+            caruselContent.innerHTML = ""
+            add_post_to_page()
+        })
 
-    let current = document.querySelector(".current");
-    current.addEventListener("click", function() {
-        window.location.assign(`selected_post.html?id=${posts[pos].id}`)
-    })
-    let next = document.querySelector(".next");
-    next.addEventListener("click", function() {
-        pos += 1
-        updatePage_desktop()
-    })
+    }
 
+    if(mobile){
+        let current = document.querySelectorAll(".current");
+        current.forEach(element => {
+            element.addEventListener("click", function() {
+                window.location.assign(`selected_post.html?id=${element.id}`)
+                console.log(element)
+            })
+        })
+    }
+    else {
+        let current = document.querySelector(".current");
+        current.addEventListener("click", function() {
+            window.location.assign(`selected_post.html?id=${posts[pos].id}`)
+        })
+    }
+
+
+    if(!mobile){
+        let next = document.querySelector(".next");
+        next.addEventListener("click", function() {
+            pos += 1
+            caruselContent.innerHTML = ""
+            add_post_to_page()
+        })
+    }
     
 }
 
-
-
-
-
-
-
 function check_screen_size(x) {
-    if(x.matches) {
-        postWrapper.innerHTML = ""
-        pos = 0
-        updatePage_mobile()
-    }
-    else{
-        postWrapper.innerHTML = ""
-        updatePage_desktop()
-    }
+    location.reload();
 }
 
 
@@ -215,4 +195,14 @@ async function comments(element) {
         console.log(err)
     }
 }
+
+morePosts.addEventListener("click", function() {
+    if(mobile){
+        pos += 3
+    }
+    add_post_to_page()
+})
+
+
+
 apiCall()
--- scripts/selected_post.js
+++ scripts/selected_post.js
@@ -4,13 +4,39 @@ const queryId = params.get("id")
 const wrapper = document.querySelector(".wrapper");
 
 
-const url = `https://myflashcard.org/wp-json/wp/v2/posts/${queryId}`;
+const baseUrl = "https://myflashcard.org/wp-json/";
+const postUrl = "wp/v2/posts"
 const commentUrl = "wp/v2/comments"
+
+const url = `https://myflashcard.org/wp-json/wp/v2/posts/${queryId}`;
 async function apiCall() {
     try {
         let respons = await fetch(url);
         let data = await respons.json();
-        wrapper.innerHTML =data.content.rendered
+        wrapper.innerHTML = ""
+        wrapper.innerHTML += `<h1>${data.title.rendered}</h1>`
+        wrapper.innerHTML += data.content.rendered
+        console.log(data)
+    }
+    catch(err) {
+        console.log(err)
+    }
+}
+
+
+
+
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+async function comments() {
+    let postId = `?post=${queryId}`
+    
+    try {
+        let respons = await fetch(baseUrl + commentUrl + postId);
+        let data = await respons.json()
+        data.forEach(comment => {
+            generate_comments(comment)
+        });
 
     }
     catch(err) {
@@ -18,4 +44,52 @@ async function apiCall() {
     }
 }
 
+const commentWrap = document.querySelector(".comments-wrap")
+let commentList = []
+function generate_comments (comment) {
+    console.log(comment)
+    let authorName = comment.author_name
+    let date  = comment.date
+    let month;
+    let day;
+
+    if (date[8] === "0") {
+        day = date[9]
+    }
+    else {
+        day = date[8] + date[9];
+    }
+
+    if (date[5] === "0") {
+        month = parseInt(date[6] - 1)
+    }
+    else {
+        month = date[5] + date[6];
+        month = parseInt(month - 1)
+    }
+    let content = comment.content.rendered
+
+    let HTML = `<div class="comments">
+                    <div class="profile">
+                        <figure class="profile_img">
+                            <img src="/images/no_img.png" alt="">
+                        </figure>
+                        <div class="comment_data">
+                            ${authorName}
+                            <p>${months[month]}
+                            ${day}.</p>
+                        </div>
+                    </div>
+                    ${content}
+                </div>`
+    commentWrap.innerHTML += HTML    
+}
+
+
+
+
+
+
+
 apiCall()
+comments()
